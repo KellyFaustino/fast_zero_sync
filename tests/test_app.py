66d26e9985e7ccc9630e8c7ea504a9a41883dcb6
kelly_faustino <kelly.faustino@cmsw.com>
2025-03-05 12:07:30 -0300

    Atualizando endpoints para usar o banco de dados real

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
+
 
 def test_read_root_return_ok_e_ola_mundo(client):
     response = client.get('/')
@@ -16,8 +18,6 @@ def teste_creat_user(client):
             'password': 'password',
         },
     )
-
-    # Voltou 201 Created ( status code correto )
     assert response.status_code == HTTPStatus.CREATED
     assert response.json() == {
         'username': 'testusername',
@@ -29,18 +29,16 @@ def teste_creat_user(client):
 def test_read_users(client):
     response = client.get('/users/')
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'testusername',
-                'email': 'test@test.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client):
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -58,7 +56,7 @@ def test_update_user(client):
     }
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted'}
